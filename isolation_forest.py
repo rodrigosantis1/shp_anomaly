196ffc2e469d952ac208a480fbbf449ff17df7a4
rodrigosantis1 <rodrigo.santis@live.com>
2020-07-14 10:52:26 -0300

    new version

--- isolation_forest.py
+++ isolation_forest.py
@@ -43,7 +43,7 @@ def temporal_distance(C_true, C_detect, max_p=None, div=1):
     return ttc.sum()/div, ctt.sum()/div
 
 # Constants
-N_RUN=3
+N_RUN=1
 
 models = [('PCA', make_pipeline(PCA())),
           ('KICA-PCA', make_pipeline(RBFSampler(n_components=100), FastICA(whiten=True), PCA(n_components=20))),
@@ -55,14 +55,18 @@ train = pd.read_csv('data/train.csv', parse_dates=['t'], index_col=0)
 test = pd.read_csv('data/test.csv', parse_dates=['t'], index_col=0)
 ft = pd.read_csv('data/faults.csv', parse_dates=['t'],index_col=0)
 df = pd.concat([train,test])
+om = pd.read_csv('data/online_monitoring.csv', parse_dates=['t'],index_col=0)
 
 # Dataset 3D visualization
-fig = plt.figure(figsize=(8,6))
+fig = plt.figure(figsize=(8,7))
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_trisurf(df['V6'],df['V5'],df[['V1','V2','V3','V4']].mean(axis=1),
                 cmap='twilight_shifted', alpha=0.5)
 ax.set_xlabel('Apparent Power'), ax.set_ylabel('LHU Inflow'), ax.set_zlabel('Average Vibration')
-plt.show()
+ax.set_xlim(1000,6095)
+ax.set_ylim(13.2,23.3)
+plt.savefig('img/dataset.jpg', dpi=600, format='jpg', bbox_inches='tight')
+
 
 # Simmulation
 results = []
@@ -105,6 +109,15 @@ for name, model in models:
             'CTT'           : ctt,
             'l'             : l
         })
+
+
+# Print results of Online Monitoring    
+C_online = (om.index - df.index.min()).astype('timedelta64[m]').astype(np.int64).sort_values()
+
+ttc, ctt = temporal_distance(C_true, C_online, div=60) # maximum delay of 12 hours
+l = abs(len(C_true) - len(C_online))
+print("{:.2f} {:.2f} {:.2f} {}".format(ttc+ctt, ttc, ctt, l))
+
     
 df_results = pd.DataFrame(results)
 df_results_group = df_results.groupby('model').agg(['mean','std'])
@@ -122,8 +135,7 @@ legend_elements = [Line2D([0], [0], color='blue', lw=1, ls='--', label='Threshol
                    Line2D([0], [0], ls='None', marker='.', color='b', label='Detection'),
                    Line2D([0], [0], ls='None', marker='x',color='r', label='Fault')]
 
-f, axs = plt.subplots(3,figsize=(10,6))
-    
+f, axs = plt.subplots(3,figsize=(12,7))
 for i, sample in enumerate(samples):
     start, end = sample[0], sample[1]
     x = score.loc[start:end].resample('5T').mean().abs()
@@ -137,8 +149,61 @@ for i, sample in enumerate(samples):
     axs[i].scatter(f.index.values, np.repeat(th, len(f)), marker='x', c='red')
     axs[i].set_xlabel('')
     axs[i].set_ylabel('Score')
-    lgd = plt.legend(handles=legend_elements, loc='lower left', framealpha=1)#, bbox_to_anchor=(1,1), loc="upper left")
-
+    axs[i].spines['right'].set_visible(False)
+    axs[i].spines['top'].set_visible(False)
+    lgd = plt.legend(handles=legend_elements, loc='lower left', framealpha=1)
     plt.tight_layout()
-    
-plt.savefig('anomalies.jpg', dpi=450,format='jpg', bbox_inches='tight')#, bbox_extra_artists=(lgd,), bbox_inches='tight')
+plt.savefig('img/anomalies.pdf', format='pdf', bbox_inches='tight')
+
+
+
+f, ax = plt.subplots(figsize=(6,4))
+x = abs(score.score.values)
+mov_avg = abs(score.score.rolling(window=48).mean().values)
+ax.plot(x, c='grey', lw=0.5)
+ax.plot(mov_avg, c='green',lw=2)
+#x[x<=th] = np.nan
+#ax.plot(x, 'k.-')
+ax.axhline(y=th, c='blue',ls='--',lw=1)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.set_xlim(0,len(test))
+ax.set_ylabel('Score')
+ax.set_xlabel('Observation')
+plt.legend(handles=[Line2D([0], [0], color='blue', lw=1, ls='--', label='Threshold'),
+                    Line2D([0], [0], color='green', lw=2, ls='-', label='Moving Average (m=48)')])
+plt.savefig('img/score.pdf', format='pdf', bbox_inches='tight')
+
+
+f, ax = plt.subplots(figsize=(6,5))
+start = ft.index[0]
+for end in ft.index[1:]:
+    l = score[start:end].score.values
+    #l = test[start:end]['V1'].cumsum().values #.drop(['V5','V6'],axis=1)
+    #l = (l - np.mean(l)) / np.std(l)
+    if (len(l) > 10):    
+        ax.plot(-l[-15:], lw=1)
+    start = end
+ax.axhline(y=th, c='blue',ls='--',lw=1)
+
+## NOT USED PLOTS
+
+
+cmaps = ['Blues','Greens','Oranges','Reds']
+cs = ['blue','green','orange','red']
+
+fig = plt.figure(figsize=(8,7))
+ax = fig.add_subplot(111, projection='3d')
+
+for i, sample in enumerate(samples):
+    start, end = sample[0], sample[1]
+    df2 = df.loc[start:end]
+    colors = plt.cm.jet(np.linspace(0,1,len(df2)))
+    ax.plot(df2['V6'],df2['V5'],df2[['V1','V2','V3','V4']].mean(axis=1), c=cs[i], alpha=0.5)
+    ax.scatter(df2['V6'],df2['V5'],df2[['V1','V2','V3','V4']].mean(axis=1),
+                    c=np.arange(len(df2)),
+                    cmap=cmaps[i], alpha=0.5)    
+    plt.figure()
+ax.set_xlabel('Apparent Power'), ax.set_ylabel('LHU Inflow'), ax.set_zlabel('Average Vibration')
+#ax.set_xlim(1000,6095)
+#ax.set_ylim(13.2,23.3)
